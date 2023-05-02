1c7f9675444e202c44cd9c696c8ac7d4b5a6dffd
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-05-02 14:04:23 -0500

    created fixed inline header and navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>My Website</h1>
+        <ul>
+          <li>About Me</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+      <div className="App-body">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +24,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
